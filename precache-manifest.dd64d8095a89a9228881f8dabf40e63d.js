36572d11d8f092a36ff2de6b70debf1b5e453817
Gwendoline Sanabria <gwensanabria@gmail.com>
2020-09-08 23:46:42 -0500

    Updates

---
+++ precache-manifest.dd64d8095a89a9228881f8dabf40e63d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7258a06569cad078309face84bb3651",
+    "url": "/React_Porftfolio/index.html"
+  },
+  {
+    "revision": "6634f5ec6925661a895c",
+    "url": "/React_Porftfolio/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "5bf8ba226d57d17cf7d7",
+    "url": "/React_Porftfolio/static/css/main.20d5d31f.chunk.css"
+  },
+  {
+    "revision": "6634f5ec6925661a895c",
+    "url": "/React_Porftfolio/static/js/2.40d9b7e6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/React_Porftfolio/static/js/2.40d9b7e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bf8ba226d57d17cf7d7",
+    "url": "/React_Porftfolio/static/js/main.6f368103.chunk.js"
+  },
+  {
+    "revision": "488ce9056cc5e0bfce45",
+    "url": "/React_Porftfolio/static/js/runtime-main.59cda9c0.js"
+  },
+  {
+    "revision": "660d4d024d91f83ead652ebf73597cc7",
+    "url": "/React_Porftfolio/static/media/Sanabria.resume.660d4d02.pdf"
+  }
+]);
